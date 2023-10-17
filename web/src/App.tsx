b257927afc29f7c0abe3e9e98af12213582b6a6f
Raymond Bian <raybbian@gmail.com>
2023-10-17 11:12:08 -0400

    created aseprite tileable template

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className={"w-screen h-screen grid place-items-center"}>
+        <p className={"text-8xl"}>
+            Raymond Bian.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
-
-export default App;
