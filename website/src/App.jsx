86ad6f04ba99eab73f3a4fd76dd1d95cfa97be2c
Raymond Bian <raybbian@gmail.com>
2023-10-22 18:10:40 -0400

    removed overlapping tile model (unused)

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -42,7 +42,7 @@ export default function App() {
             if (model.isGenerationComplete()) {
                 clearInterval(display)
             }
-        }, 0)
+        }, 100)
     }, []);
 
     return (
